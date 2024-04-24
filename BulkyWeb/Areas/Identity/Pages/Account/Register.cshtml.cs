180687f4bc619a2cb58f793fb3ad040d7a3e477e
KavishBaingane <104kbaingane@gmail.com>
2024-04-24 15:05:35 +0530

    Develop:Added company tanble to user's table , companyId added.

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -168,7 +168,13 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
                 user.PhoneNumber = Input.PhoneNumber;
                 user.Name = Input.Name;
                 user.StreetAddress  = Input.StreetAddress;
-                user.CompanyId = Input.CompanyId;
+                
+
+                if(Input.Role == SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
