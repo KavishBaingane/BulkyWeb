3a3bc601c48f0c7706e60e2a1db6fc2476f75f38
KavishBaingane <104kbaingane@gmail.com>
2024-04-26 11:10:17 +0530

    Order Summary Page

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -18,7 +18,7 @@ namespace BulkyBook.Models
         public string? State { get; set; }
         public string? PostalCode { get; set; }
         public string? StreetAddress { get; set; }
-        public string? PhoneNumber { get; set; }
+        //public string? PhoneNumber { get; set; }
         public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
