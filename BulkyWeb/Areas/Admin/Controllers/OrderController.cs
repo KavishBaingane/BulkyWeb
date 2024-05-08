11ab57c261f06dc44fb3e3881de4aa012d164702
KavishBaingane <104kbaingane@gmail.com>
2024-05-08 19:33:52 +0530

    W2:Added authorization for admin only.

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -15,7 +15,7 @@ using System.Security.Claims;
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize]
+    [Authorize(Roles =SD.Role_Admin)]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
